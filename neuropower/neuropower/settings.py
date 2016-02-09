b56043ae3e0c8125cb964bb1b30677c2752d685e
jokedurnez <joke.durnez@gmail.com>
2016-02-08 17:16:58 -0800

    static

--- neuropower/neuropower/settings.py
+++ neuropower/neuropower/settings.py
@@ -121,3 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static","static_root")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static","static_root"),
+    #os.path.join(BASE_DIR,"static_in_virtualenv")
+    #'/var/www/static_in_pro/',
+]
+
+MEDIA_URL="/media/"
+MEDIA_ROOT=os.path.join(os.path.dirname(BASE_DIR),"static","media_root")
