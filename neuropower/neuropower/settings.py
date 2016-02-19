b8fbdb96aae1bd94d459b26eff73e24eb2946c44
jokedurnez <joke.durnez@gmail.com>
2016-02-18 20:39:17 -0800

    revert to Chris' settings

--- neuropower/neuropower/settings.py
+++ neuropower/neuropower/settings.py
@@ -81,27 +81,27 @@ WSGI_APPLICATION = 'neuropower.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'sqlite3',
-        # The following settings are not used with sqlite3:
-        #'USER': 'postgres',
-        #'HOST': 'db',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        #'PORT': '5432',        # Set to empty string for default.
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': 'postgres',
+#         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': 'sqlite3',
 #         # The following settings are not used with sqlite3:
-#         'USER': 'postgres',
-#         'HOST': 'db',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-#         'PORT': '5432',        # Set to empty string for default.
+#         #'USER': 'postgres',
+#         #'HOST': 'db',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+#         #'PORT': '5432',        # Set to empty string for default.
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'postgres',
+        # The following settings are not used with sqlite3:
+        'USER': 'postgres',
+        'HOST': 'db',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',        # Set to empty string for default.
+    }
+}
 
 CACHES = {
     'default': {
