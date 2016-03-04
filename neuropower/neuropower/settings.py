208573cd636639bb4104c4d9cd32dce4edfc4dd3
jokedurnez <joke.durnez@gmail.com>
2016-03-03 21:49:48 -0800

    hyde secret key, turn on production mode

--- neuropower/neuropower/settings.py
+++ neuropower/neuropower/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@sc_pxiu+hjyf+kdex&*0n@d#@^xnvgma10=)*14innt#boy72'
+SECRET_KEY =  os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
