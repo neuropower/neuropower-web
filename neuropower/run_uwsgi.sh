8688a76bd8e7935961f139dceacaf2b8759166ec
jokedurnez <joke.durnez@gmail.com>
2016-05-09 14:24:21 -0700

    add fsl and crn environment

--- neuropower/run_uwsgi.sh
+++ neuropower/run_uwsgi.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+source activate crnenv
+source /usr/share/fsl/5.0/etc/fslconf/fsl.sh
 python manage.py makemigrations
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
