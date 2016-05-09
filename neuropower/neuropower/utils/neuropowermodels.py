43c7a25c0f31c397773ca37c3709d92e34182278
jokedurnez <joke.durnez@gmail.com>
2016-05-09 14:23:22 -0700

    added voxelsize to threshold calculator

--- neuropower/neuropower/utils/neuropowermodels.py
+++ neuropower/neuropower/utils/neuropowermodels.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import scipy
 import numpy as np
 import peakdistribution
@@ -138,11 +139,13 @@ def modelfit(peaks,pi1,exc=None,starts=1,method="RFT"):
 				'delta': par[minind]}
 	return out
 
-def threshold(peaks,pvalues,FWHM,nvox,alpha=0.05,exc=None,method="RFT"):
+def threshold(peaks,pvalues,FWHM,voxsize,nvox,alpha=0.05,exc=None,method="RFT"):
 	# only RFT
 	peakrange = np.arange(exc,15,0.001)
 	pN = 1-nulCDF(np.array(peakrange),exc=exc)
-	resels = nvox/np.product(FWHM)
+	# smoothness
+	FWHM_vox = np.asarray(FWHM)/np.asarray(voxsize)
+	resels = nvox/np.product(FWHM_vox)
 	pN_RFT = resels*np.exp(-peakrange**2/2)*peakrange**2
 	cutoff_UN = np.min(peakrange[pN<alpha])
 	cutoff_BF = np.min(peakrange[pN<alpha/len(peaks)])
