4666892cde2688abad0cbf9db1a2e1e9e02b6ecf
jokedurnez <joke.durnez@gmail.com>
2016-05-18 13:35:59 -0700

    merge migrations

--- neuropower/run_uwsgi.sh
+++ neuropower/run_uwsgi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source activate crnenv
 source /usr/share/fsl/5.0/etc/fslconf/fsl.sh
-python manage.py makemigrations --merge
+python manage.py makemigrations --merge --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 python manage.py celeryd -l info
