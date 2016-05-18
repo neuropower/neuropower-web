f5fa476d85b2279c07a8b923118ac7e0d76e6a5e
jokedurnez <joke.durnez@gmail.com>
2016-05-18 13:49:43 -0700

    if no model: white plot

--- neuropower/neuropowertoolbox/plots.py
+++ neuropower/neuropowertoolbox/plots.py
@@ -17,45 +17,48 @@ import mpld3
 def plotModel(request):
     plt.switch_backend('agg')
     sid = get_session_id(request)
+    peakdata = PeakTableModel.objects.filter(SID=sid)[::-1][0]
     mixdata = MixtureModel.objects.filter(SID=sid)[::-1][0]
     parsdata = ParameterModel.objects.filter(SID=sid)[::-1][0]
-    peakdata = PeakTableModel.objects.filter(SID=sid)[::-1][0]
-    peaks = peakdata.data
-    twocol = Paired_12.mpl_colors
-    xn = np.arange(-10,30,0.01)
-    nul = [1-float(mixdata.pi1)]*neuropowermodels.nulPDF(xn,exc=float(parsdata.ExcZ),method="RFT")
-    alt = float(mixdata.pi1)*neuropowermodels.altPDF(xn,mu=float(mixdata.mu),sigma=float(mixdata.sigma),exc=float(parsdata.ExcZ),method="RFT")
-    mix = neuropowermodels.mixPDF(xn,pi1=float(mixdata.pi1),mu=float(mixdata.mu),sigma=float(mixdata.sigma),exc=float(parsdata.ExcZ),method="RFT")
-    xn_p = np.arange(0,1,0.01)
-    alt_p = [1-float(mixdata.pi1)]*scipy.stats.beta.pdf(xn_p, float(mixdata.a), 1)+1-float(mixdata.pi1)
-    null_p = [1-float(mixdata.pi1)]*len(xn_p)
-    mpl.rcParams['font.size']='11.0'
+    if not peakdata.err == "":
+        fig=plt.figure(facecolor="white")
+    else:
+        peaks = peakdata.data
+        twocol = Paired_12.mpl_colors
+        xn = np.arange(-10,30,0.01)
+        nul = [1-float(mixdata.pi1)]*neuropowermodels.nulPDF(xn,exc=float(parsdata.ExcZ),method="RFT")
+        alt = float(mixdata.pi1)*neuropowermodels.altPDF(xn,mu=float(mixdata.mu),sigma=float(mixdata.sigma),exc=float(parsdata.ExcZ),method="RFT")
+        mix = neuropowermodels.mixPDF(xn,pi1=float(mixdata.pi1),mu=float(mixdata.mu),sigma=float(mixdata.sigma),exc=float(parsdata.ExcZ),method="RFT")
+        xn_p = np.arange(0,1,0.01)
+        alt_p = [1-float(mixdata.pi1)]*scipy.stats.beta.pdf(xn_p, float(mixdata.a), 1)+1-float(mixdata.pi1)
+        null_p = [1-float(mixdata.pi1)]*len(xn_p)
+        mpl.rcParams['font.size']='11.0'
 
-    fig,axs=plt.subplots(1,2,figsize=(14,5))
-    fig.patch.set_facecolor('None')
-    fig.subplots_adjust(hspace=.5,wspace=0.3)
-    axs=axs.ravel()
+        fig,axs=plt.subplots(1,2,figsize=(14,5))
+        fig.patch.set_facecolor('None')
+        fig.subplots_adjust(hspace=.5,wspace=0.3)
+        axs=axs.ravel()
 
-    axs[0].hist(peaks.pval,lw=0,normed=True,facecolor=twocol[0],bins=np.arange(0,1.1,0.1),label="observed distribution")
-    axs[0].set_ylim([0,3])
-    axs[0].plot(xn_p,null_p,color=twocol[3],lw=2,label="null distribution")
-    axs[0].plot(xn_p,alt_p,color=twocol[5],lw=2,label="alternative distribution")
-    axs[0].legend(loc="upper right",frameon=False)
-    axs[0].set_title("Distribution of "+str(len(peaks))+" peak p-values \n $\pi_1$ = "+str(round(float(mixdata.pi1),2)))
-    axs[0].set_xlabel("Peak p-values")
-    axs[0].set_ylabel("Density")
-    axs[1].hist(peaks.peak,lw=0,facecolor=twocol[0],normed=True,bins=np.arange(min(peaks.peak),30,0.3),label="observed distribution")
-    axs[1].set_xlim([float(parsdata.ExcZ),np.max(peaks.peak)])
-    axs[1].set_ylim([0,1])
-    axs[1].plot(xn,nul,color=twocol[3],lw=2,label="null distribution")
-    axs[1].plot(xn,alt,color=twocol[5],lw=2, label="alternative distribution")
-    axs[1].plot(xn,mix,color=twocol[1],lw=2,label="total distribution")
+        axs[0].hist(peaks.pval,lw=0,normed=True,facecolor=twocol[0],bins=np.arange(0,1.1,0.1),label="observed distribution")
+        axs[0].set_ylim([0,3])
+        axs[0].plot(xn_p,null_p,color=twocol[3],lw=2,label="null distribution")
+        axs[0].plot(xn_p,alt_p,color=twocol[5],lw=2,label="alternative distribution")
+        axs[0].legend(loc="upper right",frameon=False)
+        axs[0].set_title("Distribution of "+str(len(peaks))+" peak p-values \n $\pi_1$ = "+str(round(float(mixdata.pi1),2)))
+        axs[0].set_xlabel("Peak p-values")
+        axs[0].set_ylabel("Density")
+        axs[1].hist(peaks.peak,lw=0,facecolor=twocol[0],normed=True,bins=np.arange(min(peaks.peak),30,0.3),label="observed distribution")
+        axs[1].set_xlim([float(parsdata.ExcZ),np.max(peaks.peak)])
+        axs[1].set_ylim([0,1])
+        axs[1].plot(xn,nul,color=twocol[3],lw=2,label="null distribution")
+        axs[1].plot(xn,alt,color=twocol[5],lw=2, label="alternative distribution")
+        axs[1].plot(xn,mix,color=twocol[1],lw=2,label="total distribution")
 
-    peak_heights_string = str(round(float(mixdata.mu)/np.sqrt(parsdata.Subj),2))
-    axs[1].set_title("Distribution of peak heights \n $\delta_1$ = %s" %(peak_heights_string))
-    axs[1].set_xlabel("Peak heights (z-values)")
-    axs[1].set_ylabel("Density")
-    axs[1].legend(loc="upper right",frameon=False)
+        peak_heights_string = str(round(float(mixdata.mu)/np.sqrt(parsdata.Subj),2))
+        axs[1].set_title("Distribution of peak heights \n $\delta_1$ = %s" %(peak_heights_string))
+        axs[1].set_xlabel("Peak heights (z-values)")
+        axs[1].set_ylabel("Density")
+        axs[1].legend(loc="upper right",frameon=False)
     canvas = FigureCanvas(fig)
     response = HttpResponse(content_type='image/png')
     canvas.print_png(response)
