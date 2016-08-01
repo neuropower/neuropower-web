e94e769b4f1e8b31c92ecf79dbf833ba66151a80
jokedurnez <joke.durnez@gmail.com>
2016-08-01 11:26:20 -0500

    direct django (rather than docker) for development

--- neuropower/neuropowertools/settings.py
+++ neuropower/neuropowertools/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # own
+    'apps.designtoolbox',
     'apps.neuropowertoolbox',
     # third party
     'crispy_forms',
