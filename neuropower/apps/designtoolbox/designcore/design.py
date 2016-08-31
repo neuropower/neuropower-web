8cd9bc96eaad146789f8f6d5600756c0aec1b618
jokedurnez <joke.durnez@gmail.com>
2016-08-30 17:04:38 -0700

    debug FcCalc

--- neuropower/apps/designtoolbox/designcore/design.py
+++ neuropower/apps/designtoolbox/designcore/design.py
@@ -684,7 +684,7 @@ class GeneticAlgorithm(object):
         Qexp = np.zeros([self.stimtype,self.stimtype,self.ConfoundOrder])
         for si in range(self.stimtype):
             for sj in range(self.stimtype):
-                for r in np.arange(1,ConfoundOrder+1):
+                for r in np.arange(1,self.ConfoundOrder+1):
                     Qexp[si,sj,r-1] = self.P[si]*self.P[sj]*(self.L-r+1)
         Qmatch = np.sum(abs(Q-Qexp))
         Design["Fc"] = Qmatch
