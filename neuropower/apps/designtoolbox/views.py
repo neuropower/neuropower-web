2ec4c8f2cee7c04226c8b097a8afbaa2e1366d48
jokedurnez <joke.durnez@gmail.com>
2016-09-06 00:07:45 +0000

    small changes on server

--- neuropower/apps/designtoolbox/views.py
+++ neuropower/apps/designtoolbox/views.py
@@ -431,6 +431,7 @@ def runGA(request):
                 # Run !
                 if desdata.stop==0:
                     print("running genetic algorithm")
+                    convergence = False
                     Results = []
                     form.running = 5
                     form.save()
