c20bf7e44e257562bc2520a25d4a206de36912c2
jokedurnez <joke.durnez@gmail.com>
2016-09-15 17:53:59 -0700

    ready for deployment

--- neuropower/neuropowertools/settings.py
+++ neuropower/neuropowertools/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*','localhost']
 
 
@@ -90,27 +90,27 @@ WSGI_APPLICATION = 'neuropowertools.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 #
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'sqlite3',
-        # The following settings are not used with sqlite3:
-        #'USER': 'postgres',
-        #'HOST': 'db',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        #'PORT': '5432',        # Set to empty string for default.
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': 'postgres',
+#         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': 'sqlite3',
 #         # The following settings are not used with sqlite3:
-#         'USER': 'postgres',
-#         'HOST': 'db',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-#         'PORT': '5432',        # Set to empty string for default.
+#         #'USER': 'postgres',
+#         #'HOST': 'db',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+#         #'PORT': '5432',        # Set to empty string for default.
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'postgres',
+        # The following settings are not used with sqlite3:
+        'USER': 'postgres',
+        'HOST': 'db',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',        # Set to empty string for default.
+    }
+}
 
 ## Caches
 CACHES = {
