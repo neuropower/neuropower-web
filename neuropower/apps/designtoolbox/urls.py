3f9b5155c199c13236e8ee2af5197c1aa2552c39
jokedurnez <joke.durnez@gmail.com>
2018-01-08 09:58:02 -0800

    remove erroneous urls for control

--- neuropower/apps/designtoolbox/urls.py
+++ neuropower/apps/designtoolbox/urls.py
@@ -16,8 +16,5 @@ urlpatterns = [
     url(r'^tutorial/$',views.tutorial,name='DTutorial'),
     url(r'^methods/$',views.methods,name='DMethods'),
     url(r'^package/$',views.package,name='DPackage'),
-    url(r'^end/$',views.end_session,name='DReset'),
-    url(r'^check_long/$',views.check,name='Check'),
-    url(r'^stop/$',views.stop,name='Stop'),
-    url(r'^updatepage/$',views.updatepage,name='DUpdate'),
-        ]
+    url(r'^end/$',views.end_session,name='DReset')
+    ]
