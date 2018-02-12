b5968885ffd77408af844adc034496b3d8e0812d
jokedurnez <joke.durnez@gmail.com>
2018-02-12 14:36:11 -0800

    debug boto3

--- neuropower/settings/settings.py
+++ neuropower/settings/settings.py
@@ -184,7 +184,7 @@ STATIC_ROOT = '/var/www/static/'
 
 MEDIA_URL= "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 MEDIA_ROOT='/'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Celery config
 
